fb474cdb2b46d463b3341af125a263a02e2791b2
Avishek-45 <theabhi45@gmail.com>
2021-05-06 19:12:18 +0545

    setup done devise active_storage bootstrap install and acount model created

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+   Account.create([
+       { first_name: 'Sarh', last_name: 'Dahal' ,   username: 'sarah' ,email: 'sarah@gmail.com' ,password: 'password '}, 
+       { first_name: 'Laxmi', last_name: 'Tysen' ,   username: 'laxmi' ,email: 'laxmi@gmail.com' ,password: ' password'}, 
+       { first_name: 'Rahul', last_name: 'william' ,   username: 'rahul' ,email: 'rahul@gmail.com', password: ' password'}, 
+       { first_name: 'Sita', last_name: 'jhonson' ,   username: 'sita' ,email: 'sita@gmail.com' ,password: ' password'}, 
+       { first_name: 'Hari', last_name: 'watson' ,   username: 'hari' ,email: 'hari@gmail.com' ,password: 'password '}, 
+       { first_name: 'Ram', last_name: 'sharma' ,   username: 'ram' ,email: 'ram@gmail.com' ,password: 'password '}, 
+    ])
